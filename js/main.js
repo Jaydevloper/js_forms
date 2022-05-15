18551abc70dc77759758ce42f790241426471b77
Jaydeveloper <jayhanmirx@gmail.com>
2022-05-15 19:27:11 +0500

    vazifa2

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
+//2-vazifa
+let userName = prompt('iltimos ismingizni kiriting ');
+let userSurnamae = prompt('iltimos familiyangizni kiriting');
+let userBorm = prompt('tug`ilgan yilingizni kiriting');
 
-1-vazifa
-let a = prompt('')
-if (a%2 ==! 0){
-    alert( a + " soni " +`toq son`);
-} else if(a%2 == 0){
-    alert( a + " soni " +`juft son`);
-} else{
-    alert('iltimos son kiriting');
+if (userBorm > 18 || userBorm >= 2004){
+    alert("Assolomaleykum" + userName +" "+ userSurnamae+" " + userBorm +" siz armiyaga qabul qildingiz");
+} else if (userBorm < 18 || userBorm < 2004 ){
+    alert("Assolomaleykum " + userName+" " + userSurnamae+" siz " + userBorm +  "yoshda bo'lganigiz uchun  armiyaga qabul qilinmadingiz")
+}else {
+    alert('iltios son kiriting');
 }
-
 
 
 
